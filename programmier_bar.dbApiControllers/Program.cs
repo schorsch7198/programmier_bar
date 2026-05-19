223a384cc05b510dc99d0fae73f2d7725f6d7eb2
schorsch7198 linux <georgsteiner7@gmail.com>
2026-05-19 14:51:21 +0200

    next try to commit git ignore changes

--- programmier_bar.dbApiControllers/Program.cs
+++ programmier_bar.dbApiControllers/Program.cs
@@ -14,7 +14,7 @@ namespace programmier_bar.dbApiControllers
 			var builder = WebApplication.CreateBuilder(args);
 			builder.Services.AddCors(options =>
 				options.AddDefaultPolicy(policy => policy
-					.WithOrigins("http://localhost:5500")   // or whatever your front-end URL is
+					.WithOrigins("http://127.0.0.1:5500", "http://localhost:5500")   // or whatever your front-end URL is
 					.AllowAnyHeader()
 					.AllowAnyMethod()
 					.AllowCredentials()));
